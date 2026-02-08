67dff02167c92cb4c68425c77cf1ce26e57b653c
dbautistav <dbautistav@users.noreply.github.com>
2026-02-08 14:36:41 -0600

    Update README.md to list included tools

--- development/dev-git.sh
+++ development/dev-git.sh
@@ -7,6 +7,7 @@ sudo apt-get install -y git vim
 git config --global core.editor "vim"
 git config --global push.default simple
 git config --global color.ui true
+git config --global init.defaultBranch main
 sudo chown $USER .gitconfig
 
 echo '   [git-dev]     ...done!'
